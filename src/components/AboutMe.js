f486e1aed4ecb07e3a7d1970d8f427b3bd92f83c
David Kurniadi Weinardy <david.kurniadi.weinardy@outlook.com>
2024-01-31 14:31:26 +0100

    updated experience

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -22,7 +22,15 @@ const AboutMe = () => {
   return (
     <div className="about-me">
       <h1>About Me</h1>
-      <p>A developer with a cross-interest and passion in cultivating sustainable and cool web-solutions <br/> for your needs.</p>
+      <p>
+        A developer with a 
+        <strong> cross-interest </strong> and 
+        <strong> passion </strong> 
+        in cultivating 
+        <strong> sustainable</strong> and 
+        <strong> cool</strong> web-solutions 
+        <strong> for your needs</strong>.
+      </p>
       <div className="about-me-container">
         <div className={'cards-container'}>
           <Cards
@@ -30,9 +38,10 @@ const AboutMe = () => {
             title={'Generals'}
             image={generals}
             items={[
-              'Born: 1999',
+              `Age: ${new Date().getFullYear() - 1999}`,
+              'Born: Febuary 1999',
               'Nationality: Indonesian',
-              'Sport: Table Tennis, Rock Climbing, Futsal',
+              'Sport: Table Tennis, Bouldering, Futsal',
               'Languages: Bahasa Indonesia, English, German'
             ]}
           />
@@ -41,8 +50,7 @@ const AboutMe = () => {
             title={'Education'}
             image={edu}
             items={[
-              'White Label eCommerce - Frontend Developer | ongoing',
-              'Hamburg University of Applied Sciences - Business Information Technology | ongoing',
+              'Hamburg University of Applied Sciences - B.Sc. Business Information System | ongoing',
               'Zero to Mastery Academy - Web Developer Online Course 2022 | completed',
               'Studienkolleg Leibniz University Hanover | graduated in 2019'
             ]}
@@ -52,21 +60,22 @@ const AboutMe = () => {
             title={'Skills'}
             image={skills}
             items={[
-              'Frontend: HTML, CSS, JavaScript, TypeScript, React',
+              'Frontend: HTML, CSS, JavaScript, TypeScript, React, Tailwind, Less, Wordpress, Webflow',
               'Backend: Java, PHP, Node, PostgreSQL',
-              ''
+              'Deployment: Netlify, Docker, UNIX Environment, Webpack, Vite, Parcell',
+              'Day-to-day: Excel, Figma, Jira'
             ]}
           />
-          <div className='cards'>
-            <div className={'title'}>
-              <img src={experience} alt={'experience'} id={'experience'} />
-              <h1>Experience</h1>
-            </div>
-            <p>MALOA Hamburg | Bowl Server</p>
-            <p>PORSERA Sport Event for Indonesia Students in Germany | Creative
-              Team</p>
-            <p>The Union of Indonesian Students in Germany | Honorary Member</p>
-          </div>
+          <Cards
+            id={'exp'}
+            title={'Work Experiences'}
+            image={experience}
+            items={[
+              'White Label eCommerce | Working Student Frontend Developer | 2022 - Now',
+              'MALOA Hamburg | Working Student Server | 2019 - 2021',
+              'The Union of Indonesian Students in Hanover, Germany | Honorary Member | 2018 - 2019'
+            ]}
+          />
         </div>
       </div>
     </div>
