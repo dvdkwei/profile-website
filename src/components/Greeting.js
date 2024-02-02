8808165762cf10c6502f9833a2d59a00b274636e
David Kurniadi Weinardy <david.kurniadi.weinardy@outlook.com>
2024-02-02 11:52:04 +0100

    added animation

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -2,16 +2,16 @@ import React from "react";
 import '../styles/GreetingStyle.css';
 
 class Greeting extends React.Component {
-    render() {
-        return (
-            <div className='greeting' id="greeting">
-                <h1>David Kurniadi Weinardy</h1>
-                <p>{'web developer </>'}</p>
-                <p>business information system student 📈</p>
-                <p>based in hamburg, germany 🚢</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='greeting' id="greeting">
+        <h1>David Kurniadi Weinardy</h1>
+        <p>{'web developer </>'}</p>
+        <p>business information system student 📈</p>
+        <p>based in hamburg, germany 🚢</p>
+      </div>
+    );
+  }
 }
 
 export default Greeting;
